38b2a20839e37a573b6bf86defd68ff2e2bd5df7
Yusuke Shinyama <yusuke@shinyama.jp>
2015-05-11 14:39:49 +0900

    Added: keyword search.

--- search.py
+++ search.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+
+# usage: $ ./search.py osm.db 35.7 139.7 foo
+
 import sys
 import math
 import sqlite3
@@ -24,7 +27,7 @@ def chunk(s):
 def main(argv):
     import getopt
     def usage():
-        print 'usage: %s [-d] [-r range] file.db {lat lng|name}' % argv[0]
+        print 'usage: %s [-d] [-r range] file.db lat lng [name ...]' % argv[0]
         return 100
     try:
         (opts, args) = getopt.getopt(argv[1:], 'dr:')
@@ -36,45 +39,40 @@ def main(argv):
         if k == '-d': debug += 1
         elif k == '-r': radius = float(v)
     path = args.pop(0)
+    if len(args) < 2: return usage()
     conn = sqlite3.connect(path)
-    if 2 <= len(args):
-        lat0 = float(args.pop(0))
-        lng0 = float(args.pop(0))
-        print (lat0,lng0,radius)
-        entity = conn.cursor()
-        node = conn.cursor()
-        point = conn.cursor()
-        point.execute('SELECT nid,lat,lng FROM point WHERE '
-                      '?<=lat and ?<=lng and lat<=? and lng<=?;',
-                      (lat0-radius,lng0-radius, lat0+radius,lng0+radius))
-        eids = set()
-        for (nid,lat1,lng1) in point:
-            node.execute('SELECT eid FROM node WHERE nid=?', (nid,))
-            for (eid,) in node:
-                print (lat1,lng1), getdist((lat0,lng0),(lat1,lng1)), eid
-                if eid in eids: continue
-                eids.add(eid)
-                entity.execute('SELECT name,props FROM entity WHERE eid=?;', (eid,))
-                for (name,props) in entity:
-                    print ' ',name.encode('sjis','ignore'), props
-    else:
-        gram_entity = conn.cursor()
-        entity = conn.cursor()
-        eids = None
-        for name in args:
-            for w in chunk(name.decode('sjis')):
-                gram_entity.execute('SELECT eids FROM gram_entity WHERE w=?;', (w,))
-                for (b,) in gram_entity:
-                    a = array.array('I')
-                    a.fromstring(b)
-                    if eids is None:
-                        eids = set(a)
-                    else:
-                        eids.intersection_update(set(a))
-        for eid in eids:
-            entity.execute('SELECT name,props FROM entity WHERE eid=?;', (eid,))
-            for (name,props) in entity:
-                print ' ',name.encode('sjis','ignore'), props
+    lat0 = float(args.pop(0))
+    lng0 = float(args.pop(0))
+    print (lat0,lng0,radius)
+    entity = conn.cursor()
+    #
+    node = conn.cursor()
+    point = conn.cursor()
+    point.execute('SELECT nid,lat,lng FROM point WHERE '
+                  '?<=lat and ?<=lng and lat<=? and lng<=?;',
+                  (lat0-radius,lng0-radius, lat0+radius,lng0+radius))
+    eids = {}
+    for (nid,lat1,lng1) in point:
+        node.execute('SELECT eid FROM node WHERE nid=?', (nid,))
+        for (eid,) in node:
+            #print (lat1,lng1), getdist((lat0,lng0),(lat1,lng1)), eid
+            if eid in eids: continue
+            eids[eid] = (nid,lat1,lng1)
+    #
+    gram_entity = conn.cursor()
+    for name in args:
+        for w in chunk(name.decode('sjis')):
+            gram_entity.execute('SELECT eids FROM gram_entity WHERE w=?;', (w,))
+            for (b,) in gram_entity:
+                a = array.array('I')
+                a.fromstring(b)
+                for eid in set(eids).difference(a):
+                    del eids[eid]
+    #
+    for (eid,(nid,lat,lng)) in eids.iteritems():
+        entity.execute('SELECT name,props FROM entity WHERE eid=?;', (eid,))
+        for (name,props) in entity:
+            print nid,lat,lng, ':', name.encode('sjis','ignore'), props
     return 0
 
 if __name__ == '__main__': sys.exit(main(sys.argv))
