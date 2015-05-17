236ac83ddb27b825e9153a89791e1e0dfffe786a
Yusuke Shinyama <yusuke@shinyama.jp>
2015-05-17 19:56:52 +0900

    Address expansion.

--- addr/gen_addr.py
+++ addr/gen_addr.py
@@ -15,10 +15,12 @@ def parse(city):
     return None
 
 def e(x):
-    if isinstance(x, (list,tuple)):
+    if isinstance(x, (list,tuple,set)):
         return ','.join( e(y) for y in x )
-    else:
+    elif isinstance(x, unicode):
         return x.encode('utf-8')
+    else:
+        return str(x)
 
 # main
 def main(argv):
@@ -26,20 +28,39 @@ def main(argv):
     import fileinput
     args = argv[1:]
     d = {}
+    def add(k, v):
+        if k in d:
+            r = d[k]
+        else:
+            d[k] = r = set()
+        r.add(v)
+        return
+    #
     for row in csv.reader(fileinput.input()):
+        rgncode = int(row[1])
         pref = row[2].decode('utf-8')
         city = row[3].decode('utf-8')
         assert city, row
         x = parse(city)
         assert x, city
-        k = x[-1][:-1]
-        if k in d:
-            d[k].append((pref,city))
-        else:
-            d[k] = [(pref,city)]
-    for (k,v) in d.iteritems():
-        if len(v) < 2: continue
-        print e(k), e(v)
+        add(pref, (True,rgncode))
+        if not pref.endswith(u'道'):
+            add(pref[:-1], (False,rgncode))
+        add(city, (True,rgncode))
+        name = x[-1]
+        add(name, (True,rgncode))
+        add(name[:-1], (False,rgncode))
+    #
+    print 'ADDRESS = {'
+    for k in sorted(d):
+        v = d[k]
+        #if len(v) < 2: continue
+        codes = set( code for (_,code) in v )
+        exacts = set( exact for (exact,_) in v )
+        assert len(exacts) == 1
+        exact = list(exacts)[0]
+        print " (u'%s', %r, %r)," % (k.encode('utf-8'), exact, list(codes))
+    print '}'
     return
 
 if __name__ == '__main__': sys.exit(main(sys.argv))
