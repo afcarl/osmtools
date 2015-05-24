833f3aaba862feaed2fad2527d9dc59f1c31830b
Yusuke Shinyama <yusuke@shinyama.jp>
2015-05-25 01:31:55 +0900

    Improved search.

--- addr/parse_addr.py
+++ addr/parse_addr.py
@@ -4,10 +4,14 @@ import sys
 import sqlite3
 import re
 import reg
+import time
 import array
 from region import EXACT, KEYWORD
 
-class Trie(object):
+
+##  TrieDict
+##
+class TrieDict(object):
     
     def __init__(self):
         self.root = {}
@@ -42,7 +46,7 @@ class Trie(object):
             s += c
         return (s,v)
 
-REGION = Trie()
+REGION = TrieDict()
 for (k,v) in EXACT.iteritems():
     REGION.add(k, (True, v))
 for (k,v) in KEYWORD.iteritems():
@@ -50,46 +54,67 @@ for (k,v) in KEYWORD.iteritems():
 
 POSTAL = re.compile(r'\d{3}-?\d{4}', re.U)
 DIGIT = re.compile(r'\d+', re.U)
-WORD = re.compile(r'\w+', re.U)
+WORD = re.compile(r'[^\d\W]+', re.U)
 
-def reg_name(s):
-    def f(m): return str(reg.intkan(m.group(0)))
-    s = reg.KANDIGIT.sub(f, s).translate(reg.ALT)
-    return s
 
+##  Pred
+##
+class Pred(object): pass
+
+class PredRegion(Pred):
+    def __init__(self, name, codes):
+        self.name = name
+        self.codes = codes
+        return
+    def __repr__(self):
+        return ('<Region %r (%d codes)>' % (self.name, len(self.codes)))
+
+class PredPostal(Pred):
+    def __init__(self, v):
+        self.postal = v.replace('-','')
+        return
+    def __repr__(self):
+        return ('<Postal %s-%s>' % (self.postal[:3], self.postal[3:]))
+
+class PredWord(Pred):
+    def __init__(self, w):
+        self.word = w.translate(reg.ALT)
+        return
+    def __repr__(self):
+        return ('<Word %r>' % self.word)
+    
 def get_preds(s):
     i = 0
     while i < len(s):
         m = POSTAL.match(s, i)
         if m:
-            v = m.group(0).replace('-','')
-            yield [('P', v)]
+            yield (PredPostal(m.group(0)),)
             i = m.end(0)
             continue
         m = DIGIT.match(s, i)
         if m:
             w = m.group(0)
-            yield [('W', w)]
+            yield (PredWord(w),)
             i = m.end(0)
             continue
         m = WORD.match(s, i)
         if m:
             (k,v) = REGION.lookup(s, i)
             if v is None:
-                yield [('W', s[i:])]
+                yield (PredWord(m.group(0)),)
                 i = m.end(0)
             else:
                 (exact,codes) = v
                 if exact:
-                    yield [('C', codes)]
+                    yield (PredRegion(k, codes),)
                 else:
-                    yield [('C', codes), ('W', k)]
+                    yield (PredRegion(k, codes), PredWord(k))
                 i += len(k)
             continue
         i += 1
     return
 
-def list_addr(preds):
+def expand_preds(preds):
     def f(i):
         if i < len(preds):
             for z in f(i+1):
@@ -100,21 +125,21 @@ def list_addr(preds):
     return f(0)
 
 def search_addr(cur, preds):
-    #print 'search_addr', preds
+    print 'search_addr:', preds
     codes = None
-    for (t,v) in preds:
-        if t == 'C':
+    for pred in preds:
+        if isinstance(pred, PredRegion):
             if codes is None:
-                codes = set(v)
+                codes = set(pred.codes)
             else:
-                codes.intersection_update(set(v))
+                codes.intersection_update(set(pred.codes))
     if not codes: return None
     #print 'codes', codes
     aids = None
-    for (t,v) in preds:
-        ids1 = None
-        if t == 'W':
-            for g in reg.getgrams(v):
+    for pred in preds:
+        if isinstance(pred, PredWord):
+            ids1 = None
+            for g in reg.getgrams(pred.word):
                 cur.execute('SELECT aids FROM gram_address WHERE w=?;', (g,))
                 for (b,) in cur:
                     a = array.array('I')
@@ -123,21 +148,26 @@ def search_addr(cur, preds):
                         ids1 = set(a)
                     else:
                         ids1.intersection_update(set(a))
-            cur.execute('SELECT aid FROM address WHERE rgncode IN (%s);' %
-                        ','.join(map(str, codes)))
             if ids1:
-                ids1.intersection_update( set( aid for (aid,) in cur ) )
-        elif t == 'P':
-            cur.execute('SELECT aid FROM address WHERE postal=?;', (v,))
+                cur.execute('SELECT aid FROM address WHERE aid IN (%s) AND rgncode IN (%s);' %
+                            (','.join(map(str, ids1)), ','.join(map(str, codes))))
+                ids1.intersection_update(set( aid for (aid,) in cur ))
+            
+        elif isinstance(pred, PredPostal):
+            cur.execute('SELECT aid FROM address WHERE postal=?;', (pred.postal,))
             ids1 = set( aid for (aid,) in cur )
-        elif t == 'C':
+            
+        elif isinstance(pred, PredRegion):
             continue
-        #print 'pred', (t,v), ids1
+        
         if not ids1: break
         if aids is None:
             aids = ids1
+            print ' first: %r (%d)' % (pred, len(aids))
         else:
             aids.intersection_update(ids1)
+            print ' narrow: %r (%d)' % (pred, len(aids))
+    print ' found:', aids
     return aids
 
 def main(argv):
@@ -146,12 +176,17 @@ def main(argv):
     conn = sqlite3.connect(path)
     cur = conn.cursor()
     #
-    s = u'165-0022 東京都中野区江古田'
+    s = u'東京 中野 江古田33'
     s = reg.zen2han(s)
+    def f(m): return str(reg.intkan(m.group(0)))
+    s = reg.KANDIGIT.sub(f, s)
     preds = list(get_preds(s))
-    print preds
-    for pr in list_addr(preds):
-        print pr, search_addr(cur, pr)
+    print 'preds:', preds
+    for pr in expand_preds(preds):
+        t0 = time.time()
+        search_addr(cur, pr)
+        t1 = time.time()
+        print '  time', int((t1-t0)*1000)
     return 0
 
 if __name__ == '__main__': sys.exit(main(sys.argv))
